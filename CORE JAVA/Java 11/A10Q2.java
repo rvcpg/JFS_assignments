0fb336ff0c9cc3b053f62c17f1402c988d732d1c
Ravi <98868020+rvcpg@users.noreply.github.com>
2022-03-02 11:53:51 +0530

    Delete A10Q2.java

--- CORE JAVA/Java 11/A10Q2.java
+++
@@ -1,6 +0,0 @@
-Var keyword cannot be used in the scenarios given below:
-	*cannot be used to declare a variable without explicit INITIALIZATION.
-	*cannot be used for RETURN types.
-	*cannot be used for INSTANCE variables.
-	*cannot be used with a LAMBDA expression.
-	*cannot be used for METHOD signatures.
