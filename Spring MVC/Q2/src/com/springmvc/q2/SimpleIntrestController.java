d8122de97c8df5cd1bc0640abb9560acaa9b80ef
Ravi <98868020+rvcpg@users.noreply.github.com>
2022-03-20 10:14:06 +0530

    Add files via upload

---
+++ Spring MVC/Q2/src/com/springmvc/q2/SimpleIntrestController.java
@@ -0,0 +1,26 @@
+package com.springmvc.q2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SimpleIntrestController {
+	
+	@RequestMapping(value = "/ravi",method = RequestMethod.GET)
+	public ModelAndView getValues(){
+		
+		ModelAndView mv =new ModelAndView("simpleInterest");
+		mv.addObject("boy","Ravi");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/Submit",method = RequestMethod.POST)
+	public ModelAndView submitValue(@ModelAttribute("si") SimpleIntrest si) {
+		ModelAndView mv =new ModelAndView("simpleInterestprint");
+		return mv;
+	}
+
+}
