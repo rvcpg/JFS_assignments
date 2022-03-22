319da1e9ed5f5fc0324c5a67bf48034a7bc1b456
Ravi <98868020+rvcpg@users.noreply.github.com>
2022-03-22 11:05:30 +0530

    Add files via upload

---
+++ Spring REST/src/main/java/Q2/App.java
@@ -0,0 +1,13 @@
+package Question2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrest2Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Springrest2Application.class, args);
+    }
+}
