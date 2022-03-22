f2d7b25787e9a1dbcb3101e3533beb169d4e2791
Ravi <98868020+rvcpg@users.noreply.github.com>
2022-03-22 11:10:48 +0530

    Delete Spring REST/src/main/java directory

--- Spring REST/src/main/java/Q5/main/java/cap/q5/employeeData/DataRepo.java
+++
@@ -1,9 +0,0 @@
-package cap.q5.employeeData
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DaoRepo extends CrudRepository<Employee, Long>{
-	
-}
--- Spring REST/src/main/java/Q9/main/java/cap/q9/repository/DataRepo.java
+++
@@ -1,9 +0,0 @@
-package cap.q9.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataRepo extends CrudRepository<Customer, Long>{
-	
-}
