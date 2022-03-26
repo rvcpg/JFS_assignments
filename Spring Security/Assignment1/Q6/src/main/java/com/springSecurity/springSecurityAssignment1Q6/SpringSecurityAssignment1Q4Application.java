b01a607a5824e0bc2d077651d201e26cbb06ce09
Ravi <98868020+rvcpg@users.noreply.github.com>
2022-03-26 18:51:52 +0530

    Add files via upload

---
+++ Spring Security/Assignment1/Q6/src/main/java/com/springSecurity/springSecurityAssignment1Q6/SpringSecurityAssignment1Q4Application.java
@@ -0,0 +1,14 @@
+
+package com.springSecurity.springsecurityAssignment1Q6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAssignment1Q4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAssignment1Q4Application.class, args);
+	}
+
+}
